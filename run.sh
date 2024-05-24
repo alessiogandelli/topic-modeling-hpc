4fcd2a783f6a609d2d08e30d0dab89bd09bf64aa
alessiogandelli <alessiogandelli99@gmail.com>
2024-05-24 17:10:20 +0200

    compiler

--- run.sh
+++ run.sh
@@ -3,6 +3,10 @@
 #SBATCH --gpus-per-node=V100:1
 #SBATCH -t 01:00:00
 
+module load GCC/11.2.0
+
+export APPTAINERENV_PATH="$PATH"
+export APPTAINERENV_LD_LIBRARY_PATH="$LD_LIBRARY_PATH"
 
 
-apptainer run --nv ./container.sif python3 src/arvis_tm.py ./../data/cop_merged_original.pkl
+apptainer run --nv ./container.sif gcc python3 src/arvis_tm.py ./../data/cop_merged_original.pkl
